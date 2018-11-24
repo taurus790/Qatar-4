516cff3b5882b9b0068623d024e1caea894af1a2
Yadigar Muradov <yadigar-m@hotmail.com>
2018-11-25 00:11:38 +0100

    Added BaseClassGameEntity and BaseClassNotification

--- Engine/Factories/WorldFactory.cs
+++ Engine/Factories/WorldFactory.cs
@@ -13,9 +13,9 @@ namespace Engine.Factories
         {
             World newWorld = new World();
 
-            newWorld.AddStation(1, "28 May", 15, 15, 20);
-            newWorld.AddStation(2, "Gənclik", 30, 30, 10);
-            newWorld.AddStation(3, "Nərimanov", 45, 45, 10);
+            newWorld.AddStation(1, "28 May", 10, 10, 20);
+            newWorld.AddStation(2, "Gənclik", 100, 100, 10);
+            newWorld.AddStation(3, "Nərimanov", 150, 150, 10);
 
             return newWorld;
         }
